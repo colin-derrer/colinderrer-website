288ae14533765b2f0003909c08b83c07f3597036
Colin Derrer <colinderrer@protonmail.com>
2024-04-23 11:03:11 -0400

    Added rewriting disclaimer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen px-12 py-8 max-w-6xl flex flex-col mx-auto">
-      <div className="text-2xl font-brand">
+      <header className="text-2xl font-brand">
         <p className="text-muted-foreground text-sm">Hello, I&apos;m</p>
         <p>
           Colin Derrer, a senior software engineer & web developer at{" "}
@@ -14,7 +14,11 @@ export default function Home() {
         <p>
           If you&apos;re interested in starting a project together, let me know.
         </p>
-      </div>
+      </header>
+      <p className="text-center text-muted-foreground text-xs mt-12">
+        I&apos;m currently rewriting this website. Please check back on
+        4/26/2024.
+      </p>
     </main>
   );
 }
